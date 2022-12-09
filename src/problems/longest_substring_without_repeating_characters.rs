d9cce01c8f3233b55f1bdab99934b41e382e192d
Drachuk Eugene <eugene.d@flypika.com>
2022-12-09 12:37:35 +0300

    longest_substring_without_repeating_characters.rs

---
+++ src/problems/longest_substring_without_repeating_characters.rs
@@ -0,0 +1,37 @@
+use std::cmp::min;
+use std::collections::HashSet;
+
+struct Solution;
+
+impl Solution {
+
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let bytes = s.as_bytes();
+
+        let mut set = HashSet::new();
+        for byte in bytes {
+            set.insert(*byte);
+        }
+        let alphabet_size = set.len();
+
+        let mut max_cnt = 0;
+        for i in 0..bytes.len() {
+            set.clear();
+            for j in i..bytes.len() {
+                let byte = bytes[j];
+                if set.contains(&byte) {
+                    break;
+                } else {
+                    set.insert(byte);
+                }
+            }
+            if max_cnt < set.len() {
+                max_cnt = set.len();
+            }
+            if max_cnt >= alphabet_size {
+                break
+            }
+        }
+        return max_cnt as i32;
+    }
+}
